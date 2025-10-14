21c2d5d39f65acf6312cb5cd4a250543fa175525
retracd <29313219+retracd@users.noreply.github.com>
2025-10-13 22:50:01 -0400

    Create boilerplate footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Developed and designed by Brent Mayes.{' '}
+        <a 
+          href="https://creativecommons.org/licenses/by-nc-nd/4.0/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          CC BY-NC-ND 4.0
+        </a>
+      </p>
+    </footer>
+  );
+}

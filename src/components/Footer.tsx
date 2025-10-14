044cefd049418c745a510a5cbcb8bada73105594
retracd <29313219+retracd@users.noreply.github.com>
2025-10-14 05:12:21 -0400

    Modified footer component to match brand standards, tweaked navbar component to include underline on items

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="text-center py-2 text-xs">
       <p>
         Developed and designed by Brent Mayes.{' '}
         <a 
@@ -8,7 +8,9 @@ export default function Footer() {
           target="_blank" 
           rel="noopener noreferrer"
         >
-          CC BY-NC-ND 4.0
+        <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
+            CC BY-NC-ND 4.0
+        </span>
         </a>
       </p>
     </footer>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,22 +4,22 @@ export default function Navbar() {
   return (
     <nav className="flex justify-end items-center gap-8 px-12 py-6 text-2xl">
         <Link href="/" className="inline-block">
-            <span className="inline-block transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
+            <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
                 Home
             </span>
         </Link>
         <Link href="/about" className="inline-block">
-            <span className="inline-block transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
+            <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
                 About
             </span>
         </Link>
         <Link href="/blog" className="inline-block">
-            <span className="inline-block transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
+            <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
                 Blog
             </span>
         </Link>
         <Link href="/info" className="inline-block">
-            <span className="inline-block transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
+            <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
                 Info
             </span>
         </Link>

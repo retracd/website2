809a3abb83492fc6b39a53c2c145c1b04ad2aeb7
retracd <29313219+retracd@users.noreply.github.com>
2025-10-14 05:04:52 -0400

    Modifed navbar hover effect to be even smooooother

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,24 +2,24 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-end items-center gap-8 px-12 py-6 text-xl">
+    <nav className="flex justify-end items-center gap-8 px-12 py-6 text-2xl">
         <Link href="/" className="inline-block">
-            <span className="inline-block transition-transform duration-500 ease-out hover:skew-x-[-12deg]">
+            <span className="inline-block transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
                 Home
             </span>
         </Link>
         <Link href="/about" className="inline-block">
-            <span className="inline-block transition-transform duration-500 ease-out hover:skew-x-[-12deg]">
+            <span className="inline-block transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
                 About
             </span>
         </Link>
         <Link href="/blog" className="inline-block">
-            <span className="inline-block transition-transform duration-500 ease-out hover:skew-x-[-12deg]">
+            <span className="inline-block transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
                 Blog
             </span>
         </Link>
         <Link href="/info" className="inline-block">
-            <span className="inline-block transition-transform duration-500 ease-out hover:skew-x-[-12deg]">
+            <span className="inline-block transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(2px)]">
                 Info
             </span>
         </Link>

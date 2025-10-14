b096be51b0e52c4749989a3e6a4feff39cb328fb
retracd <29313219+retracd@users.noreply.github.com>
2025-10-14 19:46:54 -0400

    Further modified scaling so body paragraph shape stays constant (scales up and down)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
     return (
-        <main className="flex flex-col justify-center gap-20 pt-12">
-            <div className="px-12">
-                <h1 className="font-serif italic text-[18vw] leading-none font-medium tracking-tight [text-fill-color:transparent] [-webkit-text-fill-color:transparent] [text-stroke:2px_#0B0C1A] [-webkit-text-stroke:2px_#0B0C1A]">
-                    Brent Mayes
+        <main className="flex flex-col justify-center gap-[4vw]">
+            <div className="px-[3.5vw]">
+                <h1 className="font-serif italic text-[18vw] leading-none font-medium tracking-tight [text-fill-color:transparent] [-webkit-text-fill-color:transparent] [text-stroke:2px_#0B0C1A] [-webkit-text-stroke:2px_#0B0C1A] whitespace-nowrap">
+                    Mayes, Brent.
                 </h1>
             </div>
-            <div className="ml-auto mr-12 max-w-4xl">
-                <p className="text-left text-justify uppercase tracking-tight leading-tight text-3xl">
+            <div className="ml-auto mr-[3.5vw] w-[81vw] max-w-[1230px]">
+                <p className="text-left uppercase tracking-tight leading-tight [font-size:clamp(12px,2vw,30px)]">
                     Brent Mayes is a Computer Science Engineering student at the University of Michigan. 
                     With academic experience in data structures and computer security, and professional 
                     experience in cybersecurity, he is building his skills in software engineering and 

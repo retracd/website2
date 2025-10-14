6809c746642602726accd7bd81a32ab10f2d41bc
retracd <29313219+retracd@users.noreply.github.com>
2025-10-14 04:30:50 -0400

    Updated Navbar.tsx to reflect brand design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="flex justify-end items-center gap-8 px-12 py-6 text-xl">
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/blog">Blog</Link>

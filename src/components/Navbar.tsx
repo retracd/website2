50b80a8d2b489a91ad6b44a1e58c645d6224dc56
retracd <29313219+retracd@users.noreply.github.com>
2025-10-14 04:40:03 -0400

    Added slant onhover effect to navbar items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,30 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="flex justify-end items-center gap-8 px-12 py-6 text-xl">
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/info">Info</Link>
+        <Link 
+            href="/"
+            className="transition-transform duration-500 ease-out hover:skew-x-[-12deg]"
+        >
+            Home
+        </Link>
+        <Link
+            href="/about"
+            className="transition-transform duration-500 ease-out hover:skew-x-[-12deg]"
+        >
+            About
+        </Link>
+        <Link
+            href="/blog"
+            className="transition-transform duration-500 ease-out hover:skew-x-[-12deg]"    
+        >
+            Blog
+        </Link>
+        <Link
+            href="/info"
+            className="transition-transform duration-500 ease-out hover:skew-x-[-12deg]"    
+        >
+            Info
+        </Link>
     </nav>
   );
 }

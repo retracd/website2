cc59a0d36139dbf9d055a3bcd15d3b564621a9a3
retracd <29313219+retracd@users.noreply.github.com>
2025-10-16 02:38:19 -0400

    Add hyperlinks to contact info needing links

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,16 +3,24 @@ export default function About() {
         <div className="flex flex-col items-center px-6 py-6">
             <div className="w-full max-w-[800px]">
                 {/* Header */}
-                <h1 className="text-5xl font-bold mb-0">about</h1>
+                <h1 className="text-5xl font-bold">about</h1>
                 <hr className="border-navy border-2 mb-12" />
 
                 {/* Contact */}
                 <section className="mb-12">
-                    <h2 className="text-2xl font-bold">contact</h2>
-                    <div>
+                    <h2 className="text-2xl font-bold mb-1">contact</h2>
+                    <div className="space-y-1 pl-4">
                         <p>bmayes <em>at</em> umich <em>dot</em> edu</p>
-                        <p>brentmayes.com</p>
-                        <p>linkedin.com/in/brent-mayes/</p>
+                        <p>
+                            <a href="https://brentmayes.com" className="hover:underline">
+                                brentmayes.com
+                            </a>
+                        </p>
+                        <p>
+                            <a href="https://linkedin.com/in/brent-mayes/" className="hover:underline">
+                                linkedin.com/in/brent-mayes/
+                            </a>
+                        </p>
                         <p>US Citizen</p>
                     </div>
                 </section>

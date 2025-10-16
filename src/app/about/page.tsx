62f94beb64ce807c52091e790b0ae6e920eb0c28
retracd <29313219+retracd@users.noreply.github.com>
2025-10-16 02:23:20 -0400

    Create boilerplate starting code for /about/page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <div className="flex flex-col items-center px-6 py-12">
+            <div className="w-full max-w-[800px]">
+                {/* Header */}
+                <h1 className="text-4xl font-bold mb-4">about</h1>
+                <hr className="border-navy mb-12" />
+            </div>
+        </div>
+    );
+}

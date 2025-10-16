6c0cbfcd0bdc4886a4981382d5e6fc8f4714c139
retracd <29313219+retracd@users.noreply.github.com>
2025-10-16 02:29:28 -0400

    Adjustments to header and horizontal line break

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,10 @@
 export default function About() {
     return (
-        <div className="flex flex-col items-center px-6 py-12">
+        <div className="flex flex-col items-center px-6 py-6">
             <div className="w-full max-w-[800px]">
                 {/* Header */}
-                <h1 className="text-4xl font-bold mb-4">about</h1>
-                <hr className="border-navy mb-12" />
+                <h1 className="text-5xl font-bold mb-0">about</h1>
+                <hr className="border-navy border-2 mb-12" />
             </div>
         </div>
     );

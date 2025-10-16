fa06c888aecc642a695d37cc05c8da2ad01a9450
retracd <29313219+retracd@users.noreply.github.com>
2025-10-16 02:34:27 -0400

    Write content for about/contact section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,6 +5,17 @@ export default function About() {
                 {/* Header */}
                 <h1 className="text-5xl font-bold mb-0">about</h1>
                 <hr className="border-navy border-2 mb-12" />
+
+                {/* Contact */}
+                <section className="mb-12">
+                    <h2 className="text-2xl font-bold">contact</h2>
+                    <div>
+                        <p>bmayes <em>at</em> umich <em>dot</em> edu</p>
+                        <p>brentmayes.com</p>
+                        <p>linkedin.com/in/brent-mayes/</p>
+                        <p>US Citizen</p>
+                    </div>
+                </section>
             </div>
         </div>
     );

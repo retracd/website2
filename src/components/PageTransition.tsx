b3370b5fa6a5cda45d420b7e1900b6bb8f5bb1da
retracd <29313219+retracd@users.noreply.github.com>
2025-10-18 00:17:36 -0400

    Refactored PageTransition.tsx to use AnimatePrescence from the framer-motion library

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,29 +1,21 @@
 'use client';
+import {motion, AnimatePresence} from 'framer-motion';
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from 'react';
 
 export default function PageTransition({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
-    const [isVisible, setIsVisible] = useState(false);
-
-    useEffect(() => {
-        setIsVisible(false);
-
-        const timer = setTimeout(() => {
-            setIsVisible(true);
-        }, 150);
-
-        return () => clearTimeout(timer);
-    }, [pathname]); // Runs whenever route changes
-
-    useEffect(() => {
-        // Initial page load
-        setIsVisible(true)
-    }, []);
 
     return (
-        <div className={`transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
-            {children}
-        </div>
-    )
+        <AnimatePresence mode="wait">
+            <motion.div
+                key={pathname}
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1}}
+                exit={{ opacity: 0}}
+                transition={{ duration: 0.3}}
+            >
+                {children}
+            </motion.div>
+        </AnimatePresence>
+    );
 }

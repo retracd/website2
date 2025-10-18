f1e312ad7a69ea2a69d1c3bf15f41511f0b093f1
retracd <29313219+retracd@users.noreply.github.com>
2025-10-18 00:00:39 -0400

    Created PageTransition.tsx which will replace and handle fade-in effect and fade-out effect between pages

---
+++ src/components/PageTransition.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from 'react';
+
+export default function pageTransition({ children }: { children: React.ReactNode }) {
+
+    
+}

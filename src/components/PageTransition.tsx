b497b2e6884faa161e182dc99ba6589a4bd5dcd1
retracd <29313219+retracd@users.noreply.github.com>
2025-10-18 00:05:06 -0400

    Wrote logic which handles fade-in/fade-out based on initial page load or route change

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -3,6 +3,27 @@ import { usePathname } from "next/navigation";
 import { useEffect, useState } from 'react';
 
 export default function pageTransition({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+    const [isVisible, setIsVisible] = useState(false);
 
-    
+    useEffect(() => {
+        setIsVisible(false);
+
+        const timer = setTimeout(() => {
+            setIsVisible(true);
+        }, 150);
+
+        return () => clearTimeout(timer);
+    }, [pathname]); // Runs whenever route changes
+
+    useEffect(() => {
+        // Initial page load
+        setIsVisible(true)
+    }, []);
+
+    return (
+        <div className={`transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+            {children}
+        </div>
+    )
 }

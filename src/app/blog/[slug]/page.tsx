b0f5cd7307c351e40263246df5b36d2f057201d2
retracd <29313219+retracd@users.noreply.github.com>
2025-10-25 05:16:34 -0400

    Add generateStaticParams() to blog/[slug]/page.tsx and adjust title styling

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,12 @@
-import { getPostsBySlug } from "@/lib/blog";
+import { getPostsBySlug, getAllPosts } from "@/lib/blog";
 import { notFound } from 'next/navigation';
+
+export async function generateStaticParams() {
+    const posts = getAllPosts();
+    return posts.map(post => ({
+        slug: post.slug,
+    }));
+}
 
 export default async function Post({ params }: { params: { slug: string } }) {
     const post = await getPostsBySlug(params.slug);
@@ -12,8 +19,8 @@ export default async function Post({ params }: { params: { slug: string } }) {
         <div className="flex flex-col items-center px-6 py-6">
             <article className="w-full max-w-[800px]">
                 <div className="flex justify-between items-baseline">
-                    <h1 className="text-5xl font-bold">{post.title}</h1>
-                    <p className='mb-2'>{post.date}</p>
+                    <h1 className="text-5xl font-bold lowercase">{post.title}</h1>
+                    <p>{post.date}</p>
                 </div>
                 <hr className="border-navy border-2 mb-12" />
 

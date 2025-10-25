88b6895cd7e76fa8c2188eddea7e6bc6c0668a21
retracd <29313219+retracd@users.noreply.github.com>
2025-10-25 04:32:13 -0400

    Create blog post directory system and start blog/page.tsx

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Blog() {
+    return (
+        <div className="flex flex-col items-center px-6 py-6">
+            <div className="w-full max-w-[800px]">
+                {/* Header */}
+                <h1 className="text-5xl font-bold">blog</h1>
+                <hr className="border-navy border-2 mb-12" />
+            </div>
+        </div>
+    );
+}

6f2201f8021757dc221cc38dba87763a2bed9b8b
retracd <29313219+retracd@users.noreply.github.com>
2025-10-25 00:43:20 -0400

    Revert "Refactored Navbar.tsx to account for fade-out and fade-in effects. Didn't work :( coming back to this tomorrow"
    This reverts commit b7e2d8970b0920557ebe806df9b81ae37973b080.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,50 +1,28 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Navbar() {
-    const router = useRouter();
-    const [isTransitioning, setIsTransitioning] = useState(false);
-
-    const handleNavigation = (e: React.MouseEvent<HTMLAnchorElement>, href:string) => {
-        e.preventDefault();
-        setIsTransitioning(true);
-
-        setTimeout(() => {
-            router.push(href);
-            setTimeout(() => setIsTransitioning(false), 300);
-        }, 300);
-    };
-
-    return (
-        <>
-            <nav className="flex justify-end items-center gap-8 px-12 py-6 text-2xl">
-                <Link href="/" className="inline-block">
-                    <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
-                        home
-                    </span>
-                </Link>
-                <Link href="/about" className="inline-block">
-                    <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
-                        about
-                    </span>
-                </Link>
-                <Link href="/blog" className="inline-block">
-                    <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
-                        blog
-                    </span>
-                </Link>
-                <Link href="/info" className="inline-block">
-                    <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
-                        info
-                    </span>
-                </Link>
-            </nav>
-            <div
-                className={`fixed inset-0 bg-cream z-40 pointer-events-none transition-opacity duration-300 ${
-                    isTransitioning ? 'opacity-100' : 'opacity-0'}`}
-            />
-        </>
-    );
+  return (
+    <nav className="flex justify-end items-center gap-8 px-12 py-6 text-2xl">
+        <Link href="/" className="inline-block">
+            <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
+                home
+            </span>
+        </Link>
+        <Link href="/about" className="inline-block">
+            <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
+                about
+            </span>
+        </Link>
+        <Link href="/blog" className="inline-block">
+            <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
+                blog
+            </span>
+        </Link>
+        <Link href="/info" className="inline-block">
+            <span className="inline-block underline transition-transform duration-500 ease-out hover:[transform:skewX(-12deg)_translateX(3px)]">
+                info
+            </span>
+        </Link>
+    </nav>
+  );
 }

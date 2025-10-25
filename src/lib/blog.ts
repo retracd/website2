a4855a3619fd591282dd3aaa25105566afd5f4dc
retracd <29313219+retracd@users.noreply.github.com>
2025-10-25 04:41:38 -0400

    Wrote getAllPosts function which returns all metadata for all posts

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -14,3 +14,29 @@ export interface BlogPost {
     content: string;
 }
 
+export function getAllPosts() : BlogPost[] {
+    if (!fs.existsSync(postsDirectory)) {
+        return [];
+    }
+
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames
+        .filter(fileName => fileName.endsWith('.md'))
+        .map(fileName => {
+            const slug = fileName.replace(/\.md$/, '');
+            const fullPath = path.join(postsDirectory, fileName);
+            const fileContents = fs.readFileSync(fullPath, 'utf-8');
+            const { data, content } = matter(fileContents);
+        
+            return {
+                slug,
+                title: data.title,
+                date: data.date,
+                excerpt: data.excerpt,
+                content,
+            };
+        });
+
+    // Sorts posts by newest date
+    return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}

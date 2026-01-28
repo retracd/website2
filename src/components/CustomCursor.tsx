1dcd573ecac6dfd13eef5b1db23d355417f85078
retracd <29313219+retracd@users.noreply.github.com>
2026-01-28 10:25:35 +0100

    Added a `dt` variable to the LERP on the custom cursor component. I developed it on a 165hz screen and the cursor was deathly slow on lower refresh rate screens

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -5,13 +5,11 @@ export default function CustomCursor() {
     const cursorRef = useRef<HTMLDivElement>(null);
     const positionRef = useRef({ x: 0, y: 0 });
     const targetRef = useRef({ x: 0, y: 0 });
-    
     const [isHovering, setIsHovering] = useState(false);
 
     useEffect(() => {
         const handleMouseMove = (e: MouseEvent) => {
             targetRef.current = { x: e.clientX, y: e.clientY };
-
             const target = e.target as HTMLElement;
             const isClickable =
                 target.tagName === 'A' ||
@@ -22,24 +20,30 @@ export default function CustomCursor() {
             setIsHovering(isClickable);
         };
 
-        const animate = () => {
+        let lastFrameMS = 0;
+        const animate = (time: number) => {
             if (!cursorRef.current) { return; }
+            const dt = (time - lastFrameMS) / 1000;
+            lastFrameMS = time;
 
             // Lerp
-            positionRef.current.x += (targetRef.current.x - positionRef.current.x) * 0.15;
-            positionRef.current.y += (targetRef.current.y - positionRef.current.y) * 0.15;
+            positionRef.current.x += (targetRef.current.x - positionRef.current.x) * 17 * dt;
+            positionRef.current.y += (targetRef.current.y - positionRef.current.y) * 17 * dt;
 
             // Apply smoothed position
             cursorRef.current.style.left = `${positionRef.current.x}px`;
             cursorRef.current.style.top = `${positionRef.current.y}px`;
 
+            
             requestAnimationFrame(animate);
         };
 
         window.addEventListener('mousemove', handleMouseMove);
-        animate();
+        //animate();
+        const frameId = requestAnimationFrame(animate);
         
         return () => window.removeEventListener('mousemove', handleMouseMove);
+        cancelAnimationFrame(frameId);
     }, []);
 
     return (

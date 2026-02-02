b173bcbdd370889be4d6752b5d8590d99ca26897
retracd <29313219+retracd@users.noreply.github.com>
2026-02-02 11:00:41 +0100

    Updated homepage bio text to mention summer PayPal internship and current Prague study abroad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
             <div className="ml-auto mr-[3.5vw] w-[81vw] max-w-[1230px]">
                 <RisingText 
-                    text = "Brent Mayes is a Computer Science Engineering student at the University of Michigan, focusing on software engineering and security. As an Information Security Intern at MillerKnoll, he helped bolster the organization's cloud security posture by engineering and deploying automated remediation solutions. He values thoughtful design, a sensibility that aligns with MillerKnoll's heritage as the merger of two legendary design houses. When not coding, he's likely playing guitar, listening to prog rock, reading Dostoevsky, or doing some other pretentious thing like that."
+                    text = "Brent Mayes is a Computer Science Engineering student at the University of Michigan, focusing on software engineering and security. He previously helped bolster MillerKnoll's cloud security posture by engineering and deploying automated remediation solutions as an Information Security Intern. This summer, he will be heading to Scottsdale to join PayPal as a Cyber Defense Intern. For the time being, you may find him navigating a tech-focused study abroad program in Prague. When not coding, he's likely playing guitar, listening to prog rock, reading Dostoevsky, or doing some other pretentious thing like that."
                     className="text-left lowercase font-bold tracking-tight leading-tight [font-size:clamp(12px,2vw,30px)]"
                 />
             </div>

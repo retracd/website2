d8550d036aa16ddb82f29d577bf03d159f792e31
retracd <29313219+retracd@users.noreply.github.com>
2026-04-13 19:21:39 +0200

    Create settings/page.tsx and add link to it to Navbar

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: "settings",
+};
+
+export default function Settings() {
+    return (
+        <div className="flex flex-col items-center px-6 py-6">
+            <div className="w-full max-w-[800px]">
+                {/* Header */}
+                <h1 className="text-5xl font-bold">settings</h1>
+                <hr className="border-navy border-2 mb-12" />
+            </div>
+        </div>
+    );
+}
